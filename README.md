8760fec9d813da8f665af318d0d55b4b634d8c71
VladimirKobranov <josephclturok@gmail.com>
2023-12-25 12:19:57 +0700

    push

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# dotfiles
+## .dotfiles
+
+- nvim
+- fish
+- iterm2
+- tofu60
+
+Clone repo to `.config` folder
+```
+git clone https://github.com/VladimirKobranov/dotfiles.git ~/.config
+```
---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /Users/vladkobranov/anaconda3/bin/conda
+    eval /Users/vladkobranov/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
+set -g fish_term24bit 0
+eval "$(/opt/homebrew/bin/brew shellenv)"
